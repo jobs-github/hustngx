643df70c56fa3babd012ec44a510e75cd9f78094
jobs <yao050421103@gmail.com>
2016-12-03 19:31:13 +0800

    extract parallel_subrequests.c

--- ngx_wizard/ngx_wizard.py
+++ ngx_wizard/ngx_wizard.py
@@ -27,6 +27,9 @@ conf_params = 'ngx_conf_t * cf, ngx_command_t * cmd, void * conf'
 use_upstream = lambda handler: 'upstream' in handler and None != handler['upstream']
 get_uri = lambda handler: handler['uri'].replace('/', '_')
 
+cwd = os.path.split(os.path.realpath(__file__))[0]
+parallel_subrequests_uri = os.path.join(cwd, 'tpl/parallel_subrequests.c')
+
 def write_file(path, data):
     with open(path, 'w') as f:
         f.writelines(data)
@@ -137,82 +140,12 @@ def gen_parallel_call(use_parallel, handler, end):
 def gen_parallel_imp(use_parallel, md, handler):
     # "upstream"
     #     "parallel_subrequests"
-    return merge([
-        'static void __finialize(ngx_uint_t status, ngx_http_request_t *r)',
-        '{',
-        '    ngx_int_t rc = ngx_http_send_response_imp(status, NULL, r);',
-        '    ngx_http_finalize_request(r, rc);',
-        '}',
-        '',
-        'static ngx_int_t __post_upstream(ngx_http_request_t * r, void * data, ngx_int_t rc)',
-        '{',
-        '    %s_%s_ctx_t * ctx = data;' % (md, get_uri(handler)),
-        '    ++ctx->finished;',
-        '    if (NGX_OK == rc)',
-        '    {',
-        '        if (NGX_HTTP_OK != r->headers_out.status)',
-        '        {',
-        '            ++ctx->err_count;',
-        '        }',
-        '    }',
-        '    if (ctx->finished >= ctx->requests)',
-        '    {',
-        '        __finialize((ctx->requests < ctx->backends || ctx->err_count > 0) ? NGX_HTTP_NOT_FOUND : NGX_HTTP_OK, ctx->r);',
-        '    }',
-        '    return NGX_OK;',
-        '}',
-        '',
-        'static ngx_int_t __parallel_subrequests(ngx_str_t * backend_uri, ngx_http_request_t * r)',
-        '{',
-        '    // this is just a sample implement!',
-        '    ngx_http_%s_main_conf_t * mcf = %s_get_module_main_conf(r);' % (md, md),
-        '    if (!mcf)',
-        '    {',
-        '        return NGX_ERROR;',
-        '    }',
-        gen_create_ctx(md, handler),
-        '    ctx->r = r;',
-        '    ctx->backends = ngx_http_get_backend_count();',
-        '    ctx->requests = ctx->backends;',
-        '',
-        '    static ngx_http_fetch_header_t headers[] = {',
-        '            { ngx_string("Connection"), ngx_string("Keep-Alive") },',
-        '            { ngx_string("Content-Type"), ngx_string("text/plain") }',
-        '    };',
-        '    static size_t headers_len = sizeof(headers) / sizeof(ngx_http_fetch_header_t);',
-        '    ngx_http_auth_basic_key_t auth = { mcf->username, mcf->password  }; // "username" and "password" should to be in "nginx.conf"',
-        '',
-        '    ngx_http_upstream_rr_peers_t * peers = ngx_http_get_backends();',
-        '    ngx_http_upstream_rr_peer_t * peer = peers->peer;',
-        '    while (peer)',
-        '    {',
-        '        ngx_http_fetch_args_t args = {',
-        '            NGX_HTTP_GET,',
-        '            { peer->sockaddr, peer->socklen, &peer->name, peer },',
-        '            *backend_uri,',
-        '            ngx_null_string,',
-        '            { headers, headers_len },',
-        '            ngx_null_string,',
-        '            { NULL, NULL },',
-        '            { __post_upstream, ctx }',
-        '        };',
-        '        ngx_int_t rc = ngx_http_fetch(&args, &auth);',
-        '        if (NGX_OK != rc)',
-        '        {',
-        '            --ctx->requests;',
-        '        }',
-        '        peer = peer->next;',
-        '    }',
-        '    if (ctx->requests < 1)',
-        '    {',
-        '        return NGX_ERROR;',
-        '    }',
-        '    ++r->main->count;',
-        '    r->write_event_handler = ngx_http_request_empty_handler;',
-        '    return NGX_OK;',
-        '}',
-        ''
-        ]) if use_parallel(handler) else FILTER
+    return load_from_tpl(parallel_subrequests_uri, {
+        'var_ctx_t': '%s_%s_ctx_t' % (md, get_uri(handler)),
+        'var_mcf_t': 'ngx_http_%s_main_conf_t' % md,
+        'var_get_mcf': '%s_get_module_main_conf(r);' % md,
+        'var_create_ctx': gen_create_ctx(md, handler),
+        }) if use_parallel(handler) else FILTER
 
 def gen_handler_imp(addon, md, handler):
     # "action_for_request_body": ["read", "discard", "default"]
