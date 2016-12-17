1f6a83648fb9b8c700f7e424cd519dfc8b6a817f
jobs <yao050421103@gmail.com>
2016-12-17 22:07:54 +0800

    remove "IS_WIN32" & "DIM"

--- hustngx.py
+++ hustngx.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
 # email: yao050421103@gmail.com
 import sys
-import platform
 import os.path
-import re
 import datetime
 import string
 import json
 import tarfile
 import shutil
 
-gen_tm = lambda: datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-IS_WIN32 = ("Windows" == platform.system())
-DIM = '\\' if IS_WIN32 else '/'
+FILTER = '@null_string_place_holder'
+join_lines = lambda lines, dim: string.join(filter(lambda item: -1 == item.find(FILTER), lines), dim)
+merge = lambda lines: join_lines(lines, '\n')
 
-get_bin_uri = lambda name: "%s%s%s" % (name, DIM, name)
-use_py = lambda bin_uri: os.path.exists("%s.py" % bin_uri)
+gen_tm = lambda: datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+get_bin_uri = lambda name: os.path.join(name, name)
 
 def manual(): 
     print """
@@ -25,10 +23,17 @@ def manual():
         python hustngx.py nginx-1.10.0.tar.gz ngx_wizard/ngx_wizard.json
         """
 
+def get_json_data(path):
+    json_filter = lambda f: (lambda f, l: os.path.splitext(f)[1] in l)(f, ['.json'])
+    if not json_filter(path):
+        return None
+    f = open(path, 'r')
+    data = json.load(f)
+    f.close()
+    return data
 def write_file(path, data):
     with open(path, 'w') as f:
         f.writelines(data)
-        
 def copy_file(src, des):
     try:
         shutil.copy(src, des)
@@ -50,18 +55,17 @@ def untar_ngx(nginx_tar_gz, ngx_dir, mdpath):
     os.rename(tmp_ngx_dir, os.path.join(mdpath, 'nginx'))
     os.remove(tar_name)
     return True
-
 def gen_nginx_json(ngx_dir, md):
-    __health_check = string.join([
+    __health_check = merge([
         '        "health_check":',
         '        [',
         '            "check interval=5000 rise=1 fall=3 timeout=5000 type=http",',
         '            "check_http_send \\"GET /status.html HTTP/1.1\\\\r\\\\n\\\\r\\\\n\\"",',
         '            "check_http_expect_alive http_2xx"',
         '        ],'
-        ], '\n')
+        ])
     write_file(
-        os.path.join(ngx_dir, 'conf/nginx.json'), string.join([
+        os.path.join(ngx_dir, 'conf/nginx.json'), merge([
         '{',
         '    "module": "%s",' % md,
         '    "worker_connections": 1048576,',
@@ -91,8 +95,7 @@ def gen_nginx_json(ngx_dir, md):
         '        "proxy_cmds": ["/backend/test"]',
         '    }',
         '}'
-        ], '\n'))
-        
+        ]))
 def sync(ngx_dir, md_path):
     json_data = [
         {
@@ -136,33 +139,28 @@ def sync(ngx_dir, md_path):
     with open(file, 'w') as f:
         json.dump(json_data, f)
     bin_uri = get_bin_uri("sync")
-    cmd = ('python %s.py %s' if use_py(bin_uri) else '%s %s') % (bin_uri, file)
+    cmd = ('python %s.py %s') % (bin_uri, file)
     os.system(cmd)
     os.remove(file)
-
 def gen_config(ngx_dir, md):
     write_file(os.path.join(
         ngx_dir, 'Config.sh'), (
         './configure --with-cc-opt="-g3 -O0" --with-ld-opt="-lm" --prefix=/data/%s --add-module=src/addon'
         ) % md)
-    
 def gen_htpasswd(ngx_dir):
     write_file(os.path.join(
         ngx_dir, 'conf/htpasswd'), 'jobs:jobs\n')
-        
 def gen_deploy(md_path):
     bin_uri = get_bin_uri("deploygen")
-    copy_file(("%s.py" if use_py(bin_uri) else "%s") % bin_uri, md_path)
+    copy_file("%s.py" % bin_uri, md_path)
     hosts_path = os.path.join(md_path, 'hosts')
     write_file(hosts_path, '127.0.0.1')
     output = os.path.join(md_path, 'deploy.sh')
     json_path = os.path.join(md_path, 'deploy_ngx.json')
     
     tool_path = os.path.join(md_path, "deploygen")
-    cmd = ('python %s.py search %s %s' if use_py(bin_uri) else '%s %s %s') % (
-        tool_path, json_path, hosts_path)
+    cmd = 'python %s.py search %s %s' % (tool_path, json_path, hosts_path)
     write_file(output, os.popen(cmd))
-        
 def apply_hustngx(ngx_dir, addon, md, md_path):
     sync(ngx_dir, md_path)
     shutil.move(addon, os.path.join(ngx_dir, 'src/'))
@@ -170,43 +168,23 @@ def apply_hustngx(ngx_dir, addon, md, md_path):
     gen_htpasswd(ngx_dir)
     gen_deploy(md_path)
     gen_config(ngx_dir, md)
-
-def get_json_data(path):
-    json_filter = lambda f: (lambda f, l: os.path.splitext(f)[1] in l)(f, ['.json'])
-    if not json_filter(path):
-        return None
-    f = open(path, 'r')
-    data = json.load(f)
-    f.close()
-    return data
-
-def gen_addon(wizard_conf):
-    obj = get_json_data(wizard_conf)
+def gen_addon(uri):
+    obj = get_json_data(uri)
     if not obj:
         return None
-        
-    DIM = '\\' if ("Windows" == platform.system()) else '/'
-    __md_path = lambda path, obj: '%s%s%s' % (os.path.dirname(os.path.abspath(path)), DIM, obj['module'])
-    __addon_path = lambda mdpath: '%s%saddon' % (mdpath, DIM)
-    
-    mdpath = __md_path(wizard_conf, obj)
-    addon = __addon_path(mdpath)
-    
+    root = os.path.dirname(os.path.abspath(uri))
+    mdpath = os.path.join(root, obj['module'])
+    addon = os.path.join(mdpath, 'addon')
     bin_uri = get_bin_uri("ngx_wizard")
-    cmd = ('python %s.py %s' if use_py(bin_uri) else '%s %s') % (bin_uri, wizard_conf)
+    cmd = 'python %s.py %s' % (bin_uri, uri)
     os.system(cmd)
-    
     return { 'md': obj['module'], 'mdpath': mdpath, 'addon': addon }
-        
-def gen_hustngx(nginx_tar_gz, wizard_conf):
-    obj = gen_addon(wizard_conf)
+def gen_hustngx(nginx_tar_gz, uri):
+    obj = gen_addon(uri)
     if not obj:
         return False
-    
     ngx_dir = os.path.join(obj['mdpath'], 'nginx')
-    
     nginx_tar_gz = os.path.abspath(nginx_tar_gz)
-
     if not untar_ngx(nginx_tar_gz, ngx_dir, obj['mdpath']):
         return False
     apply_hustngx(ngx_dir, obj['addon'], obj['md'], obj['mdpath'])
