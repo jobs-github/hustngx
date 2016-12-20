f699e2196a0e93f70234eaa7d256ae1977399509
jobs <yao050421103@gmail.com>
2016-12-20 13:09:40 +0800

    update hustlog Config.sh

--- samples/hustlog/nginx/Config.sh
+++ samples/hustlog/nginx/Config.sh
@@ -1,1 +1,1 @@
-./configure --with-cc-opt="-g3 -O0" --with-ld-opt="-lzlog -lpthread" --prefix=/data/hustlog --add-module=src/addon
+export LD_LIBRARY_PATH=/usr/local/lib; ./configure --with-cc-opt="-g3 -O0" --with-ld-opt="-lzlog -lpthread" --prefix=/data/hustlog --add-module=src/addon
