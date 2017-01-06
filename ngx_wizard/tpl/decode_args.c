a8f1636ed4bcf2652c19f67be3dbd41fe07f735c
jobs <yao050421103@gmail.com>
2017-01-06 13:13:22 +0800

    update tpl/decode_args.c

--- ngx_wizard/tpl/decode_args.c
+++ ngx_wizard/tpl/decode_args.c
@@ -14,6 +14,7 @@ ngx_bool_t $var_decoder(ngx_http_request_t *r, $var_arg_t * args)
     memset(args, 0, sizeof($var_arg_t));
     // reset
 $var_reset
+
     ngx_str_t val = {0,0};
 $var_impl
     return true;
